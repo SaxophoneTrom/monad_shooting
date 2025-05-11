c18c58b9d532f56a1ae33921cf187d5b32e32ec8
saxophone55.eth <the_eat_cakes@zoho.com>
2025-05-11 09:11:34 +0900

    修正

--- src/components/Shooting.tsx
+++ src/components/Shooting.tsx
@@ -1447,7 +1447,7 @@ gameStateRef.current.enemies.forEach(enemy => {
   }, []);
 
   const openShare = useCallback((sentence:string) => {
-    const url = process.env.APP_URL || "https://moxie-shooting-v2.vercel.app/";
+    const url = process.env.APP_URL || "https://monad-shooting.vercel.app/";
     //const url = "https://moxie-frame-kit.vercel.app/";
     const inputSentence = sentence + "\n\nMini App by @saxophone55.eth";
     const shareUrl = `https://warpcast.com/~/compose?text=${encodeURIComponent(inputSentence)}&embeds%5B%5D=${encodeURIComponent(url)}`
